fd735a40da4535f51c0dfaabcf6ed524d71f1ae2
unknown <lucassilvaamorim1998@gmail.com>
2019-08-12 23:41:33 -0300

    Project structure

---
+++ test/authorizes_transactions/core_test.clj
@@ -0,0 +1,7 @@
+(ns authorizes-transactions.core-test
+  (:require [clojure.test :refer :all]
+            [authorizes-transactions.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
